bfbedf7b569e1bfba757e80ba56588b13d2ea84a
roelzkie <badcoder28@gmail.com>
2022-04-22 05:14:24 +0800

    Fix import path

--- saga-choreograhpy-example/booking/app/main.py
+++ saga-choreograhpy-example/booking/app/main.py
@@ -1,10 +1,10 @@
 from fastapi import Depends, FastAPI
 
-from app.config import Settings
+from app.settings import Settings
 from app.dependencies import get_settings
+from app.redis import redis
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root(settings: Settings = Depends(get_settings)):
