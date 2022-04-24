3e2716e89a89c28a7162aa9e3e6526408e319ec1
roelzkie <badcoder28@gmail.com>
2022-04-25 02:31:46 +0800

    Use the amqp_connection from fastapi app

--- saga-choreograhpy-example/booking/app/producers.py
+++ saga-choreograhpy-example/booking/app/producers.py
@@ -3,23 +3,26 @@ from aio_pika import Message
 from app.pika_client import init_pika_client
 
 
-async def event_producers(event: str, message) -> None:
-    conn = await init_pika_client()
-    async with conn:
-        # Creating channel
-        channel = await conn.channel()
+async def event_producers(app, event: str, queue_name, message) -> None:
+    conn = app.state.amqp_connection
+    # Creating channel
+    channel = await conn.channel()
 
-        # Declare exchange
-        exchange = await channel.declare_exchange(
-            'BOOKING_SERVICE_EXCHANGE',
-            type='topic'
-        )
+    # Declare exchange
+    exchange = await channel.declare_exchange(
+        'BOOKING_SERVICE_EXCHANGE',
+        type='topic',
+        durable=True
+    )
 
-        await exchange.publish(
-            Message(
-                body="{'status': 'reserved!'}".encode(),
-                content_type="application/json",
-                headers={"foo": "bar"},
-            ),
-            routing_key=event,
-        )
+    queue = await channel.declare_queue(queue_name, auto_delete=True)
+    await queue.bind(exchange, event)
+
+    await exchange.publish(
+        Message(
+            body=message.encode(),
+            content_type="application/json",
+            headers={"foo": "bar"},
+        ),
+        routing_key=event,
+    )
