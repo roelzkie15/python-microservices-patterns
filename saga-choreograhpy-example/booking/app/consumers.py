117f888fdc08759e1aa2c1edb1440a498557cb45
roelzkie <badcoder28@gmail.com>
2022-04-25 02:31:13 +0800

    Refactor consumer to basic aio_pika connection

--- saga-choreograhpy-example/booking/app/consumers.py
+++ saga-choreograhpy-example/booking/app/consumers.py
@@ -1,39 +1,42 @@
-import asyncio
-import queue
+from functools import partial
+from typing import Callable
 
 import aio_pika
+from fastapi import FastAPI
 
-from app.message_handlers import set_booking_status
-from app.pika_client import init_pika_client
-
-
-async def init_consumer() -> None:
-    loop = asyncio.get_event_loop()
-    asyncio.ensure_future(event_consumers(loop))
+from app.services import update_booking_status_from_event
 
 
-async def event_consumers(loop):
-    conn = await init_pika_client(loop)
-    async with conn:
-        # Creating channel
-        channel = await conn.channel()
+async def init_consumer(app: FastAPI) -> None:
+    # Creating channel
+    channel = await app.state.amqp_connection.channel()
+    await channel.set_qos(prefetch_count=1)
 
-        # Declare exchange
-        exchange = await channel.declare_exchange(
-            'BOOKING_SERVICE_EXCHANGE',
-            type='topic'
-        )
+    # Declare exchange
+    exchange = await channel.declare_exchange(
+        'BOOKING_SERVICE_EXCHANGE',
+        type='topic',
+        durable=True
+    )
 
-        await handle_incoming_booking_service_events(channel, exchange)
+    await handle_incoming_booking_service_events(channel, exchange)
 
 
-async def handle_incoming_booking_service_events(channel: aio_pika.Channel, exchange) -> None:
+async def handle_incoming_booking_service_events(channel: aio_pika.Channel, exchange: aio_pika.exchange.Exchange) -> None:
     # Declaring queue
     invoice_event_queue = await channel.declare_queue('INVOICE_EVENT_QUEUE', auto_delete=True)
-    await watch_from_invoice_event(exchange, invoice_event_queue, 'INVOICE_GENERATED_EVENT', set_booking_status)
+    await watch_from_invoice_event(
+        exchange, invoice_event_queue, 'INVOICE_GENERATED_EVENT',
+        update_booking_status_from_event
+    )
 
 
-async def watch_from_invoice_event(exchange, queue, event_name: str, callback) -> None:
-    await queue.bind(exchange, routing_key=event_name)
-    await queue.consume(callback)
+async def watch_from_invoice_event(exchange: aio_pika.exchange.Exchange, queue: aio_pika.queue.Queue, event_name: str, callback: Callable) -> None:
+    await queue.bind(exchange, event_name)
+    await queue.consume(partial(process_message, callback=callback))
 
+
+async def process_message(message: aio_pika.abc.AbstractIncomingMessage, callback: Callable) -> None:
+    async with message.process(ignore_processed=True):
+        await message.ack()
+        await callback(message.body)
