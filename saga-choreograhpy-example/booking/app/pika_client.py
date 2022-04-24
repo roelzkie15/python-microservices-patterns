79e10eca5eb823674b0f729679cc348fa674bc5d
roelzkie <badcoder28@gmail.com>
2022-04-25 02:32:13 +0800

    Refactor init_pika_client to assign connection on app.state.amqp_connection

--- saga-choreograhpy-example/booking/app/pika_client.py
+++ saga-choreograhpy-example/booking/app/pika_client.py
@@ -1,10 +1,13 @@
+import asyncio
+
 import aio_pika
+from fastapi import FastAPI
 
 from app.dependencies import get_settings
 
 
-async def init_pika_client(loop = None) -> None:
+async def init_pika_client(app: FastAPI) -> aio_pika.Connection:
     settings = get_settings()
-    return await aio_pika.connect_robust(
-        settings.RABBITMQ_BROKER_URL, loop=loop
+    app.state.amqp_connection = await aio_pika.connect_robust(
+        settings.RABBITMQ_BROKER_URL, loop=asyncio.get_event_loop()
     )
