9fbbceab64829b296f675ccdb0fe914b51388aa6
roelzkie <badcoder28@gmail.com>
2022-04-27 04:21:34 +0800

    Add mock data

---
+++ saga-choreograhpy-example/booking/app/mocks.py
@@ -0,0 +1,7 @@
+from app.types import Booking
+
+LIST_OF_BOOKINGS = [
+    Booking(id=1, name='john doe', status='created'),
+    Booking(id=2, name='jane doe', status='failed'),
+    Booking(id=3, name='billy joe', status='reserved'),
+]
