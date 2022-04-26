b441ef0ddea2143d85bd1024069da1d6cb52958c
roelzkie <badcoder28@gmail.com>
2022-04-27 04:20:57 +0800

    Implement strawberry endpoint and remove traditional path

--- saga-choreograhpy-example/booking/app/main.py
+++ saga-choreograhpy-example/booking/app/main.py
@@ -1,39 +1,21 @@
 
-from fastapi import Depends, FastAPI
+from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
 
 from app.amqp_client import AMQPClient
-from app.dependencies import get_settings
-from app.services import notify_user, update_booking_status_from_event
-from app.settings import Settings
+from app.schema import schema
 
 app = FastAPI()
 
+graphql_app = GraphQLRouter(schema)
 
 @app.on_event('startup')
 async def startup():
     app.state.booking_event_store = await AMQPClient('BOOKING_EVENT_STORE').init()
 
-    await app.state.booking_event_store.event_consumer(
-        update_booking_status_from_event, 'invoice.generated', 'invoice_file_event_queue'
-    )
-
-    # Creating a consumer without queue name. It will generate a random name.
-    await app.state.booking_event_store.event_consumer(
-        notify_user, 'invoice.failed',
-    )
+@app.on_event('shutdown')
+async def shutdown():
+    await app.state.booking_event_store.connection.close()
 
 
-@app.get('/')
-async def root(settings: Settings = Depends(get_settings)):
-    return {'message': 'Hello World'}
-
-
-# TODO: Remove this path later.
-@app.get('/publish')
-async def publish():
-    await app.state.booking_event_store.event_producer(
-        'BOOKING_EVENT_STORE',
-        "{'message': 'Booking reserved!'}",
-        'invoice.generated'
-    )
-    return {'message': 'published'}
+app.include_router(graphql_app, prefix='/graphql')
