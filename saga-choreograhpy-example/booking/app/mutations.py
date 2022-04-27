52d13a566407a0ab894c02bfb936576226f6993d
roelzkie <badcoder28@gmail.com>
2022-04-28 03:39:55 +0800

    Produce booking.created event after create_booking mutation call.

--- saga-choreograhpy-example/booking/app/mutations.py
+++ saga-choreograhpy-example/booking/app/mutations.py
@@ -1,18 +1,27 @@
+from uuid import uuid4
+
 import strawberry
 from strawberry.types import Info
 
+from app.amqp_client import AMQPClient
 from app.mocks import BOOKING_LIST
+from app.models import AMQPMessage
 from app.types import Booking
-from uuid import uuid4
 
 
 @strawberry.type
 class Mutation:
     @strawberry.mutation
-    def create_booking(self, name: str, info: Info) -> Booking:
+    async def create_booking(self, name: str, info: Info) -> Booking:
         booking = Booking(
             uuid=str(uuid4()),
             name=name
         )
         BOOKING_LIST.append(booking)
+
+        amqp_client: AMQPClient = info.context['amqp_client']
+        await amqp_client.event_producer(
+            'MANAGER_EVENT_STORE', 'booking.created', message=AMQPMessage(id=booking.uuid, body=booking)
+        )
+
         return booking
