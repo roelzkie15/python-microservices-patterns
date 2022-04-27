f8d36edaf9e62b280f972074e503eb5da6faa73b
roelzkie <badcoder28@gmail.com>
2022-04-28 03:03:52 +0800

    Define context_getter and include the amqp_client to GraphQLRouter

--- saga-choreograhpy-example/booking/app/main.py
+++ saga-choreograhpy-example/booking/app/main.py
@@ -7,15 +7,23 @@ from app.schema import schema
 
 app = FastAPI()
 
-graphql_app = GraphQLRouter(schema)
 
 @app.on_event('startup')
 async def startup():
-    app.state.booking_event_store = await AMQPClient('BOOKING_EVENT_STORE').init()
+    app.state.amqp_client = await AMQPClient().init()
+    await app.state.amqp_client.event_store('BOOKING_EVENT_STORE')
+
 
 @app.on_event('shutdown')
 async def shutdown():
-    await app.state.booking_event_store.connection.close()
+    await app.state.amqp_client.connection.close()
 
 
+async def get_context():
+    return {
+        'amqp_client': app.state.amqp_client,
+    }
+
+
+graphql_app = GraphQLRouter(schema, context_getter=get_context)
 app.include_router(graphql_app, prefix='/graphql')
