1e67dd2d1dea26c62293b6846c2f8dfa7e2ae156
roelzkie <badcoder28@gmail.com>
2022-05-02 02:18:28 +0800

    Separate BookingAppEngine

--- saga-choreograhpy-example/booking/app/db.py
+++ saga-choreograhpy-example/booking/app/db.py
@@ -1,17 +1,11 @@
 import fire
 from sqlmodel import SQLModel, create_engine
 
+# This is needed
+# https://sqlmodel.tiangolo.com/tutorial/create-db-and-table/#sqlmodel-metadata-order-matters
 from app import models
 from app.dependencies import get_settings
 
+
 settings = get_settings()
 engine = create_engine(settings.DB_URL, echo=True)
-
-
-class BookingEngine:
-    def create_db_and_tables(self):
-        SQLModel.metadata.create_all(engine)
-
-
-if __name__ == '__main__':
-    fire.Fire(BookingEngine)
