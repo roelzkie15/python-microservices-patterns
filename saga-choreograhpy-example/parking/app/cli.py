b0b14ac3c308b681e829f80df8b92c1766d84649
roelzkie <badcoder28@gmail.com>
2022-06-04 22:01:40 +0800

    sc: Add DictMixin to parking models

--- saga-choreograhpy-example/parking/app/cli.py
+++ saga-choreograhpy-example/parking/app/cli.py
@@ -10,21 +10,19 @@ class AppCLI(object):
     async def create_parking_slot(self, name: str, status: str | None = 'available'):
         with Session() as session:
             ps = await create_parking_slot(session, name=name, status=status)
-            return dict((col, getattr(ps, col)) for col in ps.__table__.columns.keys())
+            return ps.to_dict()
 
     async def parking_slot_list(self):
         with Session() as session:
             ps_list = await parking_slot_list(session)
             return [
-                dict((col, getattr(ps, col))
-                for col in ps.__table__.columns.keys())
-                for ps in ps_list
+                ps.to_dict() for ps in ps_list
             ]
 
     async def parking_slot_details(self, uuid: str):
         with Session() as session:
             ps = await parking_slot_details(session, uuid)
-            return dict((col, getattr(ps, col)) for col in ps.__table__.columns.keys())
+            return ps.to_dict()
 
 
 if __name__ == '__main__':
--- saga-choreograhpy-example/parking/app/models.py
+++ saga-choreograhpy-example/parking/app/models.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Dict
 
 from pydantic import BaseModel
 from sqlalchemy import Column, String
@@ -6,13 +6,19 @@ from sqlalchemy import Column, String
 from app.db import Base
 
 
-class ParkingSlot(Base):
+class DictMixin:
+    def to_dict(self) -> Dict:
+        return dict((col, getattr(self, col)) for col in self.__table__.columns.keys())
+
+
+class ParkingSlot(Base, DictMixin):
     __tablename__ = 'parking_slots'
 
     uuid = Column(String, primary_key=True, unique=True, index=True)
     name = Column(String, nullable=False)
 
     status = Column(String, nullable=False, server_default='pending')
+
 
 class AMQPMessage(BaseModel):
     id: str
