50769fb2046ff4db2626a38b633fc430249d1d5e
roelzkie <badcoder28@gmail.com>
2022-06-04 22:13:06 +0800

    so: Setup booking service

---
+++ saga-orchestration-example/booking/app/main.py
@@ -0,0 +1,25 @@
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+
+
+async def startup_event():
+    pass
+
+
+async def shutdown_event():
+    pass
+
+
+async def root(request):
+    return JSONResponse({'message': 'Booking server is running'})
+
+routes = [
+    Route('/', root),
+]
+
+app = Starlette(
+    routes=routes,
+    on_startup=[startup_event],
+    on_shutdown=[shutdown_event]
+)
