1f40d8d07fc8fffa995effbe068567d6133904ab
roelzkie <badcoder28@gmail.com>
2022-06-13 02:47:04 +0800

    so-billing: Add more CLI functions

--- saga-orchestration-example/billing/app/cli.py
+++ saga-orchestration-example/billing/app/cli.py
@@ -1,7 +1,7 @@
 import fire
 
 from app.db import Session
-from app.services import billing_request_list
+from app.services import billing_request_list, create_billing_request, billing_request_details_by_reference_no
 
 
 class AppCLI(object):
@@ -10,6 +10,16 @@ class AppCLI(object):
         with Session() as session:
             br_list = await billing_request_list(session)
             return [br.to_dict() for br in br_list]
+
+    async def billing_request_details_by_reference_no(self, reference_no: str):
+        with Session() as session:
+            br = await billing_request_details_by_reference_no(session, reference_no)
+            return br.to_dict()
+
+    async def create_billing_request(self, reference_no: str):
+        with Session() as session:
+            br = await create_billing_request(session, reference_no)
+            return br.to_dict()
 
 
 if __name__ == '__main__':
