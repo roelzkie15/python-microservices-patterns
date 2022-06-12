62d9b304fa7b1071eb99079014b3cc69f2c32f55
roelzkie <badcoder28@gmail.com>
2022-06-13 02:48:00 +0800

    so-billing: Create create_billing_request and billing_request_details_by_reference_no functions

--- saga-orchestration-example/billing/app/services.py
+++ saga-orchestration-example/billing/app/services.py
@@ -1,9 +1,25 @@
 
 from typing import List
-from uuid import uuid4
 
 from app.db import Session
 from app.models import BillingRequest
+
+
+async def create_billing_request(session: Session, reference_no: str) -> BillingRequest:
+    br = BillingRequest(
+        # Default total to 100.00
+        total=100.0,
+        reference_no=reference_no
+    )
+    session.add(br)
+    session.commit()
+    session.refresh(br)
+
+    return br
+
+
+async def billing_request_details_by_reference_no(session: Session, reference_no: str) -> BillingRequest:
+    return session.query(BillingRequest).filter(BillingRequest.reference_no == reference_no).one()
 
 
 async def billing_request_list(session: Session) -> List[BillingRequest]:
