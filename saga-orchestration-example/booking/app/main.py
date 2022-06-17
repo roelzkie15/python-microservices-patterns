553ed9dbab5a468406b20e62c4a8f3fb9cc1dc05
roelzkie <badcoder28@gmail.com>
2022-06-18 03:49:56 +0800

    so-booking: Simplified main.py

--- saga-orchestration-example/booking/app/main.py
+++ saga-orchestration-example/booking/app/main.py
@@ -3,15 +3,6 @@ from starlette.responses import JSONResponse
 from starlette.routing import Route
 
 
-async def startup_event():
-    # TODO: Watch billing.paid event.
-    pass
-
-
-async def shutdown_event():
-    pass
-
-
 async def root(request):
     return JSONResponse({'message': 'Booking server is running'})
 
@@ -20,7 +11,5 @@ routes = [
 ]
 
 app = Starlette(
-    routes=routes,
-    on_startup=[startup_event],
-    on_shutdown=[shutdown_event]
+    routes=routes
 )
