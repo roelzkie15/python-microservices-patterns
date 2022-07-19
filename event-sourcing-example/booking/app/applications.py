77849e8230e87b25b1e65edce34aa2c9e88359e1
roelzkie <badcoder28@gmail.com>
2022-07-19 13:31:20 +0800

    Enable booking application to store events via cli

--- event-sourcing-example/booking/app/applications.py
+++ event-sourcing-example/booking/app/applications.py
@@ -1,11 +1,10 @@
 from uuid import UUID
 
-from booking.app.domainmodels import Booking
+from app.domainmodels import Booking
 from eventsourcing.application import Application
 
 
 class Bookings(Application):
-
     def create_booking(self, parking_slot_ref_no: str) -> UUID:
         booking = Booking(parking_slot_ref_no=parking_slot_ref_no)
         self.save(booking)
@@ -13,11 +12,19 @@ class Bookings(Application):
 
     def reserve_booking(self, booking_id: UUID) -> Booking:
         booking: Booking = self.get_booking(booking_id)
+
+        if booking.status == "reserved":
+            raise ValueError(f"Booking ID: {booking.id} is already {booking.status}.")
+
         booking.reserve()
         return self._update_booking(booking)
 
     def complete_booking(self, booking_id: UUID) -> Booking:
         booking: Booking = self.get_booking(booking_id)
+
+        if booking.status == "completed":
+            raise ValueError(f"Booking ID: {booking.id} is already {booking.status}.")
+
         booking.complete()
         return self._update_booking(booking)
 
--- event-sourcing-example/booking/app/cli.py
+++ event-sourcing-example/booking/app/cli.py
@@ -1,13 +1,23 @@
 from uuid import UUID
+
 import fire
-from booking.app.applications import Bookings
+from app.applications import Bookings
 
 
 class AppCLI(object):
-
     def create_booking_request(self, parking_slot_ref_no: str) -> UUID:
         booking_app = Bookings()
         return booking_app.create_booking(parking_slot_ref_no=parking_slot_ref_no)
+
+    def reserve_booking(self, booking_id: str) -> UUID:
+        booking_app = Bookings()
+        booking = booking_app.reserve_booking(booking_id=booking_id)
+        return booking.id
+
+    def complete_booking(self, booking_id: str) -> UUID:
+        booking_app = Bookings()
+        booking = booking_app.complete_booking(booking_id=booking_id)
+        return booking.id
 
 
 if __name__ == "__main__":
