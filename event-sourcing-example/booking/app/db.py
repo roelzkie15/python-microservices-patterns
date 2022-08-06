97ca646ad2bbb542cb261185a9b99068fc0a4c01
roelzkie <badcoder28@gmail.com>
2022-08-07 02:07:49 +0800

    es: Map SQLAlchemy to projector database

--- event-sourcing-example/booking/app/db.py
+++ event-sourcing-example/booking/app/db.py
@@ -2,34 +2,14 @@ import contextlib
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session, scoped_session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 from app import settings
 
-engines = {
-    "event_store": create_engine(
-        settings.EVENT_STORE_DATABASE_URL, logging_name="event_store"
-    ),
-    "read_db": create_engine(settings.READ_DATABASE_URL, logging_name="read_db"),
-}
+engine = create_engine(settings.PROJECTOR_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
-class RoutingSession(Session):
-    def get_bind(self, mapper=None, clause=None):
-        # if mapper and mapper.class_.__name__ == "BookingReplica":
-        #     return engines["read_db"]
-        if self._flushing:
-            return engines["event_store"]
-        else:
-            return engines["read_db"]
-
-
-SessionLocal = scoped_session(
-    sessionmaker(autocommit=False, autoflush=False, class_=RoutingSession)
-)
-
-EventStoreBase = declarative_base()
-ReadDBBase = declarative_base()
+ProjectorBase = declarative_base()
 
 
 @contextlib.contextmanager
