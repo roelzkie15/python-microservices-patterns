548c96a23471b2fcb57b406dbdf57f40663ccf9b
roelzkie <badcoder28@gmail.com>
2022-08-07 02:06:38 +0800

    es: Update cli to use process_runner for event sourcing application subscriptions

--- event-sourcing-example/booking/app/cli.py
+++ event-sourcing-example/booking/app/cli.py
@@ -1,23 +1,30 @@
 from uuid import UUID
 
 import fire
-from app.applications import Bookings
+
+from app.applications import Bookings, process_runner
 
 
 class AppCLI(object):
     def create_booking_request(self, parking_slot_ref_no: str) -> UUID:
-        booking_app = Bookings()
-        return booking_app.create_booking(parking_slot_ref_no=parking_slot_ref_no)
+        with process_runner() as runner:
+            booking_app = runner.get(Bookings)
+            result_id = booking_app.create_booking(
+                parking_slot_ref_no=parking_slot_ref_no
+            )
+        return result_id
 
     def reserve_booking(self, booking_id: str) -> UUID:
-        booking_app = Bookings()
-        booking = booking_app.reserve_booking(booking_id=booking_id)
-        return booking.id
+        with process_runner() as runner:
+            booking_app = runner.get(Bookings)
+            booking = booking_app.reserve_booking(booking_id=booking_id)
+            return booking.id
 
     def complete_booking(self, booking_id: str) -> UUID:
-        booking_app = Bookings()
-        booking = booking_app.complete_booking(booking_id=booking_id)
-        return booking.id
+        with process_runner() as runner:
+            booking_app = runner.get(Bookings)
+            booking = booking_app.complete_booking(booking_id=booking_id)
+            return booking.id
 
 
 if __name__ == "__main__":
