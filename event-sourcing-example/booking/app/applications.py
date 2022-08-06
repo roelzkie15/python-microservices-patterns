9012dd5dbe4dbb17d8a82faa189ac3cc59dd6e4b
roelzkie <badcoder28@gmail.com>
2022-08-07 02:07:15 +0800

    es: Update Bookings application

--- event-sourcing-example/booking/app/applications.py
+++ event-sourcing-example/booking/app/applications.py
@@ -1,7 +1,15 @@
+import contextlib
 from uuid import UUID
 
-from app.domainmodels import Booking
 from eventsourcing.application import Application
+from eventsourcing.dispatch import singledispatchmethod
+from eventsourcing.system import (ProcessApplication, SingleThreadedRunner,
+                                  System)
+
+from app.amqp_client import AMQP, AMQPClient
+from app.domainmodels import Booking
+from app.models import AMQPMessage
+from app.services import create_booking, update_booking_status_by
 
 
 class Bookings(Application):
@@ -12,24 +20,23 @@ class Bookings(Application):
 
     def reserve_booking(self, booking_id: UUID) -> Booking:
         booking: Booking = self.get_booking(booking_id)
-
-        if booking.status == "reserved":
-            raise ValueError(f"Booking ID: {booking.id} is already {booking.status}.")
-
+        self._status_checker(booking, "reserved")
         booking.reserve()
         return self._update_booking(booking)
 
     def complete_booking(self, booking_id: UUID) -> Booking:
         booking: Booking = self.get_booking(booking_id)
-
-        if booking.status == "completed":
-            raise ValueError(f"Booking ID: {booking.id} is already {booking.status}.")
-
+        self._status_checker(booking, "completed")
         booking.complete()
         return self._update_booking(booking)
 
     def get_booking(self, booking_id: UUID) -> Booking:
         booking = self.repository.get(booking_id)
+        return booking
+
+    def _status_checker(self, booking: Booking, status: str):
+        if booking.status == status:
+            raise ValueError(f"Booking ID: {booking.id} is already {booking.status}.")
         return booking
 
     def _update_booking(self, booking: Booking) -> Booking:
