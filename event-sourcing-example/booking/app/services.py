52eec624832ee6e5fc087e4832dce41fcf0afc32
roelzkie <badcoder28@gmail.com>
2022-08-10 04:33:10 +0800

    es: add new service to get booking list

--- event-sourcing-example/booking/app/services.py
+++ event-sourcing-example/booking/app/services.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from app.db import Session
 from app.models import Booking
 
@@ -18,6 +20,11 @@ def create_booking(domain_uuid: str, parking_slot_ref_no: str, status: str) -> B
 def get_booking_by_domain_uuid(domain_uuid: str) -> Booking:
     with Session() as session:
         return session.query(Booking).filter(Booking.domain_uuid == domain_uuid).one()
+
+
+def get_booking_list() -> List[Booking]:
+    with Session() as session:
+        return session.query(Booking).all()
 
 
 def update_booking_status_by(domain_uuid: str, status: str) -> Booking:
